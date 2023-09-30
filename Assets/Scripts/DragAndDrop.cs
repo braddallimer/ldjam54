c2e08ecdd3bcac141e86d60a47337435b337ab29
Brad Dallimer <braddallimer@gmail.com>
2023-09-30 14:57:25 +0100

    Mouse controls finished.

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -7,6 +7,7 @@ public class DragAndDrop : MonoBehaviour
 {
     [SerializeField] string rayTag;
     [SerializeField] BlockInstance selectedBlock;
+    [SerializeField] BlockHolder lastBlockHolder;
 
     private void Update()
     {
@@ -32,34 +33,88 @@ public class DragAndDrop : MonoBehaviour
 
                 if (selectedBlock != null)
                 {
+                    Debug.Log("1) Player is holding block...");
+
                     if (targetHolder != null)
                     {
-                        if (targetHolder.heldBlock != null)
-                            targetHolder.ReplaceBlock(selectedBlock);
+                        Debug.Log("2) ...over a block holder...");
 
-                        else
+                        // If the holder being triggered has a held block
+                        if (targetHolder.heldBlock != null)
+                        {
+                            Debug.Log("3) ...that has a held block...");
+
+                            // if the selected block comes from a previously triggered holder
+                            if (lastBlockHolder != null)
+                            {
+                                Debug.Log("4) ...being swapped from another holder.");
+
+                                targetHolder.SwapBlockFromOtherHolder(lastBlockHolder);
+                                lastBlockHolder = null;
+                            }
+
+                            // if there is no previously triggered holder
+                            else
+                            {
+                                Debug.Log("4) ...being replaced.");
+
+                                targetHolder.ReplaceBlock(selectedBlock);
+                            }
+                        }
+
+                        else // if the holder being triggered has no held block
+                        {
+                            Debug.Log("2) ...and adding it to a holder");
+
+                            if(lastBlockHolder != null)
+                                lastBlockHolder.RemoveBlockFromHolder();
+
                             targetHolder.AddBlockToHolder(selectedBlock);
+                            lastBlockHolder = null;
+                        }
 
                         SetSelectedBlock(null);
                     }
 
                     else 
                     {
+                        Debug.Log("2) ..they tried placing it over the void. Returning to pool.");
+
                         // return block to pool
                     }
                 }
 
                 else
                 {
+                    Debug.Log("1) Player is not holding a block...");
+
                     if (targetHolder != null)
                     {
+                        Debug.Log("2) ...hovering over a holder...");
+
                         if (targetHolder.heldBlock != null)
                         {
+                            Debug.Log("3) ...with a held block, they select it.");
+
                             SetSelectedBlock(targetHolder.heldBlock);
-                            targetHolder.RemoveBlockFromHolder();
+                            lastBlockHolder = targetHolder;
                         }
                     }
                 }
+            }
+
+            else if(selectedBlock != null)
+            {
+                Debug.Log("..they tried placing it over the void. Returning to pool.");
+
+                if(lastBlockHolder != null)
+                {
+                    selectedBlock.transform.position = lastBlockHolder.transform.position;
+                    selectedBlock = null;
+                    lastBlockHolder = null;
+                }
+
+                // return block to pool
             }
         }
     }
